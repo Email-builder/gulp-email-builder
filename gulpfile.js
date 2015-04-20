d8471329be7b8249d36ec745442f6ae47a2310a4
Steven John Miller <steven.jmiller@gmail.com>
2015-04-20 14:14:30 +1000

    Initial stuff

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var emailBuilder = require('./lib/emailBuilder');
+
+gulp.task('lint', function() {
+  return gulp.src(['./lib/*.js', 'gulpfile.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
