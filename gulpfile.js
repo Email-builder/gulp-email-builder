132626403986fed00a8d42a8c99aaa0faccd19db
Steven John Miller <steven.jmiller@gmail.com>
2015-04-20 14:20:34 +1000

    Set up task to get started

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var emailBuilder = require('./lib/emailBuilder');
 
+gulp.task('emailBuilder', function() {
+  return gulp.src(['./test/*.js'])
+    .pipe(emailBuilder());
+});
+
 gulp.task('lint', function() {
   return gulp.src(['./lib/*.js', 'gulpfile.js'])
     .pipe(jshint())
